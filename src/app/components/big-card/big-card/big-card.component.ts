c45f239032a922b9d73598fdcf8c799e0eaeca26
carecarestinga <carecarestinga@gmail.com>
2024-11-04 13:13:30 -0300

    Adicionando Components e Estrutura

---
+++ src/app/components/big-card/big-card/big-card.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './big-card.component.html',
+  styleUrl: './big-card.component.css'
+})
+export class BigCardComponent {
+
+  @Input()
+  photoCover:string =""
+  @Input()
+  cardTitle:string= ""
+  @Input()
+  cardDescription:string =""
+  @Input()
+  Id:string="0"
+
+}
